85ce2faa0c51f56b791f84f56ef0a724dbf9a2bd
Benny Malengier <benny.malengier@gmail.com>
2015-02-15 11:02:12 +0100

    Working line follow algorithm

---
+++ sketches/les07_lijnvolgen/DistSens.h
@@ -0,0 +1,243 @@
+ /*
+Bibliotheek om met afstandsensoren te werken
+Library to work with distance sensors
+
+
+  A distance sensor is activated by creating an instance of the DistSens class 
+  and passing the desired trigger and echo pin to the attach() method.
+  
+  You can now request a distance measurement in several ways.
+  The methods are: 
+    attach(pintrig, pinecho) - what pins to listen on
+    stop()                   - deactivate, all distance measurements will return 0.
+    start()                  - activate again after a deactivate
+    setMinMax(min, max)      - set minimum and maximum distance to measure. Default: 3.5cm and 50cm
+    setResolution(res)       - set lowest interval to allow remeasure of distance in microsec
+                               default = 250000UL = 250 millisec = 1/4 sec
+                               A measurement before this time has past will return the old distance
+    distSimple()             - distance measurement that will block the sketch for an unknown amount
+                               of time
+    distTimeout()            - distance measurement that will block the sketch maximally for the
+                               time needed for sound to go to maximum distance set by setMinMax + 1ms
+                               This time is 2*max/(speed of sound) + 1
+    distNoblock()            - distance measurement that will return in less than 1ms a value.
+                               the last measurement is returned as long as a new measurement was not
+                               performed. This method will only return correct results if called in a
+                               loop() function that uses no delay and calls this function every time. 
+                               Use this function if you have several sensors to act upon, and don't 
+                               want the distance sensor blocking updates needed for the other sensors
+*/
+
+#ifndef DISTSENS_H
+#define DISTSENS_H
+
+#include <Arduino.h> 
+
+
+//snelheid van geluid in lucht in cm/micros
+#define SPEED_SOUND 0.034
+
+class DistSens
+{
+public:
+  DistSens(){
+    //set defaults
+    minafstand          =  50;
+    maxafstand          =  3.5;
+    //hoe vaak afstand meten in microseconds
+    measure_resolution  = 250000UL;  //= 250 ms = 1/4s
+    afstand             = 0;
+    laatste_afst_meas   = 0UL;
+    timeout_echo        = (2* maxafstand+1) / SPEED_SOUND;
+    dotrig              = false;
+    inecholoc           = false;
+  };
+  void attach(int pintrig, int pinecho){
+    pinMode(pintrig, OUTPUT); trigpin=pintrig;
+    pinMode(pinecho, INPUT);  echopin=pinecho;
+    start();
+  };
+  void stop(){active=false;afstand=0.;};
+  void start(){active=true;afstand=0.;};
+  void setMinMax(float minafst, float maxafst){
+    minafstand = minafst;
+    maxafstand = maxafst;
+    timeout_echo = (2* maxafstand+1) / SPEED_SOUND;
+  };
+  void setResolution(unsigned long res) {
+    measure_resolution = res;
+  };
+
+public:
+  /*** Meten van afstand op twee pinnen, waarden tussen min/max teruggeven
+       Deze functie blokkeert je sketch.
+       Geen controle op de duurtijd van de afstandsmeting!
+    **/
+  float distSimple(){
+    if (this->active) {
+      timemicro = micros();
+      if (timemicro-laatste_afst_meas < measure_resolution){
+        //nog geen nieuwe meting doen! Oude afstand teruggeven
+        return afstand;
+      }
+      int duration;
+      digitalWrite(trigpin, HIGH); // klaarmaken voor puls
+      delayMicroseconds(1000);     // 1 ms wachten
+      digitalWrite(trigpin, LOW);  // puls afgevuurd !!
+      duration = pulseIn(echopin, HIGH); //wacht op echo in millisec
+      //antwoord in millisec, met snelheid geluid berekenen we afstand 
+      afstand =  (duration/2.) / (1000*SPEED_SOUND);
+      laatste_afst_meas = micros();
+      if (afstand < minafstand || afstand > maxafstand){
+          afstand = 0.;
+      }
+      return afstand;
+    } else return 0.;
+  };
+  /*** Meten van afstand op twee pinnen, waarden tussen min/max teruggeven
+       Deze functie blokkeert je sketch.
+       Duurtijd is maximaal de duur die geluid nodig heeft om maxafst te meten + 1ms
+    **/
+  float distTimeout(){
+    if (this->active) {
+      timemicro = micros();
+      if (timemicro-laatste_afst_meas < measure_resolution){
+        //nog geen nieuwe meting doen! Oude afstand teruggeven
+        return afstand;
+      }
+      digitalWrite(trigpin, HIGH); // klaarmaken voor puls
+      delayMicroseconds(1000);     // 1 ms wachten
+      digitalWrite(trigpin, LOW);  // puls commando gegeven!!
+      unsigned long echodurationstart = micros();
+      bool contwait = true;
+      inecholoc = false;
+      unsigned char echoval;
+      
+      while (contwait) {
+        //tijd voor echo begint te lopen als echoPin HIGH is
+        echoval = digitalRead(echopin);
+        if (echoval == HIGH) {
+          //// puls afgevuurd !! start timing
+          echodurationstart = micros();
+          inecholoc = true;
+          contwait = false;
+        } else if (micros() - echodurationstart > 800UL) {
+            //iets fout, dit duurt te lang! jump out of loop
+          contwait = false;
+          inecholoc = false;
+        }
+      }
+      unsigned long echoduration = 0UL;
+      while (inecholoc) {
+        //we zijn aan het wachten op echo
+        echoval = digitalRead(echopin);
+        //Als pin LOW wordt is echo aangekomen
+        if (echoval == LOW) {
+          //echo ontvangen, bereken de duur
+          echoduration = micros()-echodurationstart;
+          inecholoc = false;
+        } else if (micros()-echodurationstart > timeout_echo) {
+          //lang genoeg gewacht! Stop.
+          echoduration = 0UL;
+          inecholoc = false;
+        }
+      }
+      laatste_afst_meas = micros();
+      afstand = (echoduration/2.) * SPEED_SOUND;
+      if (afstand < minafstand || afstand > maxafstand){
+          afstand = 0.;
+      }
+      return afstand;
+    } else return 0.;
+  };
+  
+  /*** Meten van afstand op twee pinnen, waarden tussen min/max teruggeven
+       Deze functie blokkeert je sketch niet, je hebt een antwoord binnen 1ms.
+       Zolang geen nieuwe afstand berekend is, krijg je de waarde van de oude afstand
+       terug.
+    **/
+  float distNoblock(){
+    timemicro=micros();
+    bool updated_dist = false;
+    // meet afstand eens elke xx seconds
+    // 5 mogelijkheden:
+    //  1. nog niet nodig een nieuwe meting te doen
+    //  2. maak klaar om meting te doen
+    //  3. stuur commando om puls uit te sturen
+    //  4. controleer of puls uitgestuurd is
+    //  5. puls gestuurd, wacht op echo
+    if (timemicro-laatste_afst_meas < measure_resolution){
+      // nog niet lang genoeg sinds vorige meting, niets doen
+      dotrig = true;
+      inecholoc = false;
+    } else if (!inecholoc && dotrig == true){
+      dotrig = false;
+      // Klaarmaken om puls uit te sturen
+      digitalWrite(trigpin, HIGH);
+    } else if (!inecholoc && timemicro-laatste_afst_meas > measure_resolution + 100UL){
+      //lang genoeg gewacht, bepaal nieuwe afstand, emit sound (8x40kHz pulses):
+      echodurationstart = micros();
+      digitalWrite(trigpin, LOW);
+      //catch echo, determine afstand
+      bool contwait = true;
+      while (contwait) {
+        //timing starts running when pin reads HIGH
+        if (digitalRead(echopin) == HIGH) {
+          //start timing
+          echodurationstart = micros();
+          contwait = false;
+          inecholoc = true;
+        } else if (micros() - echodurationstart > 800UL) {
+          //timeout, jump out of loop
+          contwait = false;
+          inecholoc = false;
+          echoduration = 0UL;
+          updated_dist = true;
+        }
+      }
+    } else if (inecholoc) {
+      //we are waiting for the echo, we test the echo pin
+      //if we read LOW, timing ends
+      if (digitalRead(echopin) == LOW) {
+        //echo received, new echoduration value
+        echoduration = timemicro-echodurationstart;
+        inecholoc = false;
+        updated_dist = true;
+      } else if (timemicro-echodurationstart > timeout_echo) {
+        echoduration = 0UL;
+        inecholoc = false;
+        updated_dist = true;
+      }
+    }
+    if (updated_dist) {
+      afstand = (echoduration/2.) * SPEED_SOUND;
+      if (afstand < minafstand || afstand > maxafstand){
+        afstand = 0.;
+      }
+      //set time of this dist meas, so it does not happen again too fast
+      laatste_afst_meas = micros();
+    }
+    return afstand;
+  }
+private:
+  int           trigpin;
+  int           echopin;
+  bool          active;
+  float         minafstand;
+  float         maxafstand;
+  unsigned long timeout_echo;
+  unsigned long measure_resolution;
+  unsigned long timemicro;
+  bool          dotrig;
+  bool          inecholoc;
+  unsigned long echodurationstart;
+  unsigned long echoduration;
+  
+  float         afstand;
+  unsigned long laatste_afst_meas;
+};
+
+
+
+#endif
+
---
+++ sketches/les07_lijnvolgen/les07_lijnvolgen.ino
@@ -0,0 +1,249 @@
+/******************************/
+/* Echo device om afstand te meten
+/*  Geavanceerd: zonder de code te blokkeren, we kunnen dus
+/*  verder rijden en draaien terwijl we afstand meten
+/*  Hier rijden we en ontwijken we alle objecten.
+/******************************/
+
+#include <Servo.h>
+#include "DistSens.h"
+ 
+Servo servo1;
+Servo servo2;
+DistSens distsens;
+
+// positie servo's. Start: stilstaan 
+// waarde in percenten -100 .. 100
+int servL = 0;
+int servR = 0;
+#define servoLpin 9
+#define servoRpin 10
+int correctieL = 0;
+int correctieR = 5;
+
+// bijhouden tijd
+unsigned long timesec,   timesecbegin  =0UL;
+unsigned long timemilli, timemillibegin=0UL;
+unsigned long timemicro, timemicrobegin=0UL;
+
+// alles rond afstand:
+#define trigPin 12
+#define echoPin 13
+//maximum afstand in cm die we willen zien
+#define MAX_AFSTAND 50.
+//minimum afstand die sensor kan zien in cm
+#define MIN_AFSTAND 3.5
+//variabelen om mee te rekenen
+float afstand=0;
+
+// alles rond lijnsensor
+// pas volgende aan voor aantal sensoren die je hebt. 
+// Hier voor twee sensoren, een links en een rechts
+#define ANALOGSENS false
+#define lijnLpin  2
+#define lijnRpin  3
+#define nrlijnpin 2
+int lijnpins[]  = {lijnLpin, lijnRpin};
+#define ZWART 0
+#define WIT   1
+int lijnsens[] = {ZWART, ZWART};  // waarde per sensor
+int statuslijn;
+// Wat kunnen we zien met de 2 lijnsensoren
+#define LS_LIJNMIDDEN 0
+#define LS_LIJNLINKS  1
+#define LS_LIJNRECHTS 2
+#define LS_ZWARTVELD  3
+
+//algemene zaken autorobot
+int statusrobot;
+#define ROBOT_LIJNVOLG 0
+#define ROBOT_REDDER   1
+#define ROBOT_ONTWIJK  2
+int lijnstategie;
+#define LS_WAGGEL  0
+#define LS_TRAAG   1
+#define LS_SNEL    2
+
+//andere variabelen
+int munt=0;
+
+void setup() {
+  distsens.attach(trigPin, echoPin);
+  distsens.setMinMax(MIN_AFSTAND, MAX_AFSTAND);
+  servo1.attach(servoLpin);
+  servo2.attach(servoRpin);
+  set_servos(servL, servR); //startsnelheid
+  //pins lijnsensor hebben geen setup nodig!
+  dotime();                 //tijd bijhouden
+  randomSeed(analogRead(0));
+  
+  //we beginnen in modus lijnvolg
+  statusrobot = ROBOT_LIJNVOLG;
+  //lijn volgen door traag te gaan
+  lijnstategie = LS_TRAAG;
+  //geen afstand meten in lijnvolg
+  distsens.stop();
+  
+}
+
+void loop() {
+  dotime();
+  // gebruik hier een van de afstandsmeting methoden
+  //afstand = distsens.distSimple();
+  //afstand = distsens.distTimeout();
+  afstand = distsens.distNoblock();
+  leeslijn();
+  
+  if (statusrobot == ROBOT_ONTWIJK) {
+    if (afstand == 0) {
+      //niets te zien, rij voorruit
+      vooruit();
+      munt = random(2);
+    } else if (afstand < 18) {
+      //iets voor onze neus, hard draaien
+      if (munt == 0) { linkshard();
+      } else {         rechtshard();
+      }
+    } else {
+      //vertraag tot aan 10cm
+      set_servos(afstand/MAX_AFSTAND*100, afstand/MAX_AFSTAND*100);
+    }
+  }
+  
+  if (statusrobot == ROBOT_LIJNVOLG) {
+    if (lijnstategie == LS_TRAAG) {
+      traaglijnvolgen();
+    } else {
+      //nog schrijven!!
+      stopauto();
+    }
+  }
+}
+
+void leeslijn()
+{
+  for (int i=0;i<nrlijnpin;i++){
+    if (ANALOGSENS) {
+      if (analogRead(lijnpins[i]) < 500) {
+        //wit
+        lijnsens[i] = WIT;
+      } else {
+        //wit
+        lijnsens[i] = ZWART;
+      };
+    } else {
+      //digitaal zwart of niets
+      lijnsens[i] = digitalRead(lijnpins[i]);
+    }
+  }
+  verwerklijn();
+}
+
+void verwerklijn()
+{
+  if      (lijnsens[0] == WIT   && lijnsens[nrlijnpin-1] == WIT) 
+    statuslijn = LS_LIJNMIDDEN;
+  else if (lijnsens[0] == ZWART && lijnsens[nrlijnpin-1] == WIT)
+    statuslijn = LS_LIJNLINKS;
+  else if (lijnsens[0] == WIT   && lijnsens[nrlijnpin-1] == ZWART)
+    statuslijn = LS_LIJNRECHTS;
+  else
+    //beide lezen zwart.
+    statuslijn = LS_ZWARTVELD;
+}
+
+void traaglijnvolgen(){
+    if (statuslijn == LS_LIJNMIDDEN) {
+      vooruit(20);
+    } else if (statuslijn == LS_LIJNLINKS) {
+      linkshard(10);
+      //niet overshooten
+    } else if (statuslijn == LS_LIJNRECHTS) {
+      rechtshard(10);
+      //niet overshooten
+    } else if (statuslijn == LS_ZWARTVELD) {
+      //oeps. probeer te herstellen 
+      rechtshard(10);
+      //niet overshooten
+    }
+}
+
+void snellijnvolgen(){
+    if (statuslijn == LS_LIJNMIDDEN) {
+      vooruit();
+    } else if (statuslijn == LS_LIJNLINKS) {
+      //niet overshooten
+      achteruit(10);
+      delay(500);
+      //nu actie
+      links();
+    } else if (statuslijn == LS_LIJNRECHTS) {
+      //niet overshooten
+      achteruit(10);
+      delay(500);
+      //nu actie
+      rechts();
+    } else if (statuslijn == LS_ZWARTVELD) {
+      //oeps. probeer te herstellen 
+      //niet overshooten
+      achteruit(10);
+      delay(500);
+      //nu actie
+      rechtshard();
+    }
+}
+
+/* Opslaan van de tijd
+ */
+void dotime()
+{
+  timemilli = millis()-timemillibegin;
+  timemicro = micros()-timemicrobegin;
+  timesec = timemilli/1000;
+}
+void resettime()
+{
+    timemillibegin = timemilli;
+    timemicrobegin = timemicro;
+    timemilli = 0UL; timemicro=0UL; timesec=0UL;
+}
+
+/* De servo's snelheden geven in percent van de
+   maximale snelheid.
+ */ 
+void set_servos(int links, int rechts) 
+{
+  //testen van de input
+  if (links > 100) links = 100;
+  if (links < -100) links = -100;
+  if (rechts > 100) rechts = 100;
+  if (rechts < -100) rechts = -100;
+  //waarde sturen naar de servo als verschillend van huidige
+  if (links != servL) {
+    servo1.write(90-links*90/100.);
+    servL = links;
+  }
+  if (rechts != servR) {
+    //opgelet, rechts staat tegengesteld, dus maal -1 !!
+    servo2.write(90+rechts*90/100.);
+    servR = rechts;
+  }
+}
+
+/* handige korte functies voor typische beweging
+ */
+ void vooruit()         {set_servos( 100,  100); }
+ void vooruit(int v)    {set_servos(   v+correctieL,    v+correctieR); }
+ void achteruit()       {set_servos(-100, -100); }
+ void achteruit(int v)  {set_servos(  -(v+correctieL),  -(v+correctieR)); }
+ void links()           {set_servos(   0,  100); }
+ void links(int v)      {set_servos(   0,    (v+correctieR)); }
+ void rechts()          {set_servos( 100,    0); }
+ void rechts(int v)     {set_servos(   (v+correctieL),    0); }
+ void stopauto()        {set_servos(   0,    0); }
+ void linkshard()       {set_servos(-100,  100); }
+ void linkshard(int v)  {set_servos(  -(v+correctieL),    (v+correctieR)); }
+ void rechtshard()      {set_servos( 100, -100); }
+ void rechtshard(int v) {set_servos(   (v+correctieL),   -(v+correctieR)); }
+ 
+
